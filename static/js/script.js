8ba0c9a5b194cbcda6bc97db82b77fddb44f56bf
spartiwa-asu <spartiwa@asu.edu>
2026-02-08 22:48:36 -0700

    Add css and js pages

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const currentLocation = window.location.pathname;
+  const navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach(link => {
+    if (link.getAttribute('href') === currentLocation) {
+      link.classList.add('active');
+    }
+  });
+});
